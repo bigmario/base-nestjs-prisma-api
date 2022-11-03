4d3f4dc237da505561a95d53d5bf6f5091fa0cc7
Mario Castro <mariocastro.pva@gmail.com>
2022-11-03 16:43:21 -0400

    fixed users seeder

--- prisma/seeder/users.seeder.ts
+++ prisma/seeder/users.seeder.ts
@@ -109,4 +109,5 @@ export async function createUsers(prismaClient: PrismaServiceType) {
       primaryPhone: '+5804140000000',
     },
   });
+  await prismaClient.$queryRaw`ALTER SEQUENCE user_id_seq restart 2`;
 }
